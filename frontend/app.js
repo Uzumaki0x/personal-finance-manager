f9e2d3262353d1db17bf58bdc2fe1e86b4865642
Dev Sharma <shringan66@gmail.com>
2026-09-03 23:27:44 +0530

    build dynamic transaction dashboard

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,8 @@
 console.log("Radhe Radhe");
+function formatCurrency(amount){
+    return `₹${amount.toLocaleString("en-IN")}`
+}
 const heading = document.querySelector("h1");
-console.log(heading);
 heading.textContent = "My Finance Dashboard" ;
 const transactions = [
     {
@@ -28,51 +30,67 @@ const transactions = [
     type : "income",
     }
 ]
-const transactionList = document.querySelector(".transactions-section");
-for(const transaction of transactions){
+const transactionList = document.querySelector(".transaction-list");
+function renderTransaction(transaction){
     const item = document.createElement("article");
     item.classList.add("transaction");
+    if(transaction.type=="income"){
+        item.classList.add("income");
+    }
+    else{
+        item.classList.add("expense");
+    }
     const merchantElement = document.createElement("p");
+    merchantElement.classList.add("merchant");
     const categoryElement = document.createElement("p");
+    categoryElement.classList.add("category");
     const amountElement = document.createElement("p");
+    amountElement.classList.add("amount");
     const currencyElement = document.createElement("p");
+    currencyElement.classList.add("currency");
     const dateElement = document.createElement("p");
+    dateElement.classList.add("date");
     merchantElement.textContent = transaction.merchant ;
     categoryElement.textContent = transaction.category ;
-    amountElement.textContent = `₹${transaction.amount}` ;
+    if(transaction.type==="income"){
+    amountElement.textContent =  `+${formatCurrency(transaction.amount)}` ;
+    }
+    else{
+        amountElement.textContent = `-${formatCurrency(transaction.amount)}` ;
+    }
     currencyElement.textContent = transaction.currency ;
     dateElement.textContent = transaction.date ;
+    const transactionInfo = document.createElement("div");
+    transactionInfo.classList.add("transaction-info");
+    const transactionDetail = document.createElement("div");
+    transactionDetail.classList.add("transaction-details");
+    transactionInfo.append(merchantElement,categoryElement);
+    transactionDetail.append(amountElement,currencyElement,dateElement); 
     item.append(
-    merchantElement,
-    categoryElement,
-    amountElement,
-    currencyElement,
-    dateElement
+    transactionInfo,transactionDetail
     );
     transactionList.append(item);
-    console.log(item);
 }
-let totalIncome = 0 ;
-for (const transaction of transactions){
-    if(transaction.type === "income" ){
-        totalIncome+=transaction.amount ;
+for(const transaction of transactions){
+    renderTransaction(transaction);    
+}
+const totalIncome = transactions.reduce((total,transaction)=>{
+    if(transaction.type==="income"){
+    return total+transaction.amount;
+}
+    return total;
+},0);
+const totalExpense = transactions.reduce((total,transaction)=>{
+    if(transaction.type==="expense"){
+        return total+transaction.amount ;
     }
-}
-console.log(totalIncome);
-let totalExpense = 0 ;
-for ( const transaction of transactions ){
-    if(transaction.type === "expense"){
-        totalExpense+=transaction.amount;
-    }
-}
+    return total; 
+},0)
 const cards = document.querySelectorAll(".summary-card");
-console.log(cards);
 const balance = cards[0].querySelector("p");
-console.log(totalExpense);
-let totalBalance = totalIncome - totalExpense ;
-console.log(totalBalance);
-balance.textContent = totalBalance ;
 const income = cards[1].querySelector("p");
-income.textContent = totalIncome ;
 const expense = cards[2].querySelector("p");
-expense.textContent = totalExpense;
+let totalBalance = totalIncome - totalExpense ;
+balance.textContent = formatCurrency(totalBalance) ;
+income.textContent = formatCurrency(totalIncome);
+expense.textContent = formatCurrency(totalExpense);
